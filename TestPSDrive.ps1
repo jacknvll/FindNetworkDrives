f5e234e673f5b650893cd1a815fbe5e6299937f8
Jack <jacknvll@gmail.com>
2020-01-28 12:22:56 +1100

    Update TestDrive
    tests for HKU Ps Drive. creates if not existant. Used a Do Until Loop rather than If/Else.

--- TestPSDrive.ps1
+++ TestPSDrive.ps1
@@ -1,14 +1,32 @@
 #This is a small script to test if PSDrive exists.
 #If it does exist, do not create.
 #If it doesn't exist, create.
-
 #Value 1 is a known mapped PsDrive, and Value 2 does not exist by default:
-$Value1 = Get-PSDrive | Where-Object {$_.root -like "HKEY_CURRENT_USER"}
-$Value2 = Get-PSDrive | Where-Object {$_.root -like "HKEY_USERS"}
-if ($Value1) 
+#$Value1 = Get-PSDrive | Where-Object {$_.root -like "HKEY_CURRENT_USER"}
+<#if ($Value1) 
     {Write-Host "PsDrive exists"} else 
     {Write-Host "PsDrive does not exist"}
 if ($Value2) 
     {Write-Host "PsDrive exists"} else 
     {Write-Host "PsDrive does not exist"}
 #Testing is successful.
+#>
+<#
+function Test-Existance {
+    $ValueCheck = Get-PSDrive | Where-Object {$_.root -like "HKEY_USERS"}
+        if ($ValueCheck.Root) {return 'exists'}
+        else {
+            New-PSDrive HKU Registry HKEY_USERS
+            return 'created'
+        }
+}
+#>
+function Test-Existance {
+    $ValueCheck = Get-PSDrive | Where-Object {$_.root -like "HKEY_USERS"}
+    do {
+        New-PSDrive HKU Registry HKEY_USERS -Scope Global
+        $ValueCheck = Get-PSDrive | Where-Object {$_.root -like "HKEY_USERS"}
+    } until ($ValueCheck.Root)
+}
+#Added Global Scope as drive was not available after the test function. 
+#Persist parameter only works on Remote FileSystems.
