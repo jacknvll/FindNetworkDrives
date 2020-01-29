c986a1ac43bf4fd8476c47a036b2b477290d74e7
Jack <jacknvll@gmail.com>
2020-01-29 22:24:28 +1100

    Fixes
    File renames, cleaned up the TestHiveExistance function. Tested Local script, works good.

--- Local_FindNetworkDrives.ps1
+++ Local_FindNetworkDrives.ps1
@@ -1,15 +1,23 @@
 #Import functions
-. .\TestExistance.ps1 
-#Local Test
+. .\TestHiveExistance.ps1
+Import-Module ActiveDirectory
+
+#Potential Parameter/s:
+#$person = 'jack.neville'
+
+#Variable Definitions:
+$Error.Clear()
 $WorkingFolder = $PWD
-#Test for HiveDrive, and create if missing:
-Test-Existance
-#Define the username:
-$person = 'jack.neville'
-#Navigate to the hive location:
-[string]$Sid = (Get-ADUser -Identity $person).sid
-[string]$location = "HKU:\" + $Sid + "\Network"
-Set-Location $location
+$HKU = (Test-HiveExistance HKEY_USERS).Name
+#[string]$Sid = (Get-ADUser -Identity $person).sid   --- replaced by $manual for testing
+[string]$manual = 'S-1-5-21-3794749596-1985790629-174587685-144204' #SID of $person, for when im not on the network and to do testing.
+[string]$location = $HKU + ":\" + $manual + "\Network"
+
 #List the network drive letters and targets:
-Get-ItemProperty * | Format-Table pschildname,remotepath
+Set-Location $location
+$Results = Get-ItemProperty * | Format-Table pschildname,remotepath
+
+#Result and Return
 Set-Location $WorkingFolder
+Remove-PSDrive $HKU
+return $Results
--- TestHiveExistance.ps1
+++ TestHiveExistance.ps1
@@ -7,16 +7,19 @@ function Test-HiveExistance {
         $Type
     )
     $Error.Clear()
-    $ValueCheck = Get-PSDrive -PSProvider Registry | Where-Object {$_.root -like $Type} # -or $_.Name -like "Hive1"
-    if ($ValueCheck.root -notlike "$Type"){
+    $ValueCheck = Get-PSDrive -PSProvider Registry | Where-Object {$_.root -like $Type} #There can only be one.
+    if (!$ValueCheck){
         $n = 1
         do {
             [string]$Name = "Hive"+$n
-            try {New-PSDrive -Name $Name -PSProvider Registry -Root $Type -Scope Global -ErrorAction Stop}
+            try {[void](New-PSDrive -Name $Name -PSProvider Registry -Root $Type -Scope Global -ErrorAction Stop)}
             catch [System.Management.Automation.SessionStateException] {$n++}
             $ValueCheck = Get-PSDrive -PSProvider Registry | Where-Object {$_.root -like $Type}    
         } while ($ValueCheck.Root -notlike "$Type" -and $ValueCheck.Name -notlike "$Name")
+    $Final = Get-PSDrive -Name $Name
+    return $Final
     } else {
-        Write-Host "$Type already exists with the name $($ValueCheck.Name)"
+        return $ValueCheck
     }
+    
 }
