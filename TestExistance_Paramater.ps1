722c2047e65df6f1b830a4ea08ac82dcae69b142
Jack <jacknvll@gmail.com>
2020-01-29 21:32:19 +1100

    Error Action - Update and Solution
    Changed validation set, added if statement against PsDrive root existing, added try/catch for PsDrive name existing. do until runs until both conditions are met.

--- TestExistance_Paramater.ps1
+++ TestExistance_Paramater.ps1
@@ -2,18 +2,21 @@ function Test-HiveExistance {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$TRUE)]
-        [ValidateSet("HKEY_USERS","HKEY_CURRENT_USER","HKEY_CURRENT_CONFIG")]
+        [ValidateSet("HKEY_USERS","HKEY_CLASSES_ROOT","HKEY_CURRENT_CONFIG")]
         [string]
         $Type
     )
-    $ValueCheck = Get-PSDrive -PSProvider Registry
-    $n = 1
-    do {
-        [string]$Name = "Hive"+$n
-        New-PSDrive -Name $Name -PSProvider Registry -Root $Type -Scope Global
-        $ValueCheck = Get-PSDrive -PSProvider Registry
-        $n++
-    } until (($ValueCheck.Root -like "$Type" -and $ValueCheck.Name -ne $Name) -or $n -lt 4)
+    $Error.Clear()
+    $ValueCheck = Get-PSDrive -PSProvider Registry | Where-Object {$_.root -like $Type} # -or $_.Name -like "Hive1"
+    if ($ValueCheck.root -notlike "$Type"){
+        $n = 1
+        do {
+            [string]$Name = "Hive"+$n
+            try {New-PSDrive -Name $Name -PSProvider Registry -Root $Type -Scope Global -ErrorAction Stop}
+            catch [System.Management.Automation.SessionStateException] {$n++}
+            $ValueCheck = Get-PSDrive -PSProvider Registry | Where-Object {$_.root -like $Type}    
+        } while ($ValueCheck.Root -notlike "$Type" -and $ValueCheck.Name -notlike "$Name")
+    } else {
+        Write-Host "$Type already exists with the name $($ValueCheck.Name)"
+    }
 }
-#Added Global Scope as drive was not available after the test function. 
-#Persist parameter only works on Remote FileSystems.
