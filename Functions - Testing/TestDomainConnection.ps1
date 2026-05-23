81e47c96ff1e7cbe9e5f16cd8f25c4c2650ee53a
Jack <jacknvll@gmail.com>
2026-05-24 08:52:17 +1000

    Parameterize Domain

--- Functions - Testing/TestDomainConnection.ps1
+++ Functions - Testing/TestDomainConnection.ps1
@@ -1,8 +1,14 @@
 #Test you have a stable connection to the DC when searching for a domain account.
 function Test-DomainConnectivity
 {
+[CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$TRUE)]
+        [string]
+        $DomainFQDN
+    )
     try {
-        $Connectivity = (Test-Connection 'domain.com' -Count 4 -ErrorAction SilentlyContinue | Measure-Object).Count
+        $Connectivity = (Test-Connection $DomainFQDN -Count 4 -ErrorAction SilentlyContinue | Measure-Object).Count
     }
     catch {Continue}
 
