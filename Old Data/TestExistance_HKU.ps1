9db88b7f8b0106b101ed0de782a3d5919237bd64
Jack <jacknvll@gmail.com>
2026-05-24 08:54:49 +1000

    Refactor Test-Existance function for PSDrive creation

--- Old Data/TestExistance_HKU.ps1
+++ Old Data/TestExistance_HKU.ps1
@@ -1,26 +1,7 @@
 #This is a small script to test if PSDrive exists.
 #If it does exist, do not create.
 #If it doesn't exist, create.
-#Value 1 is a known mapped PsDrive, and Value 2 does not exist by default:
-#$Value1 = Get-PSDrive | Where-Object {$_.root -like "HKEY_CURRENT_USER"}
-<#if ($Value1) 
-    {Write-Host "PsDrive exists"} else 
-    {Write-Host "PsDrive does not exist"}
-if ($Value2) 
-    {Write-Host "PsDrive exists"} else 
-    {Write-Host "PsDrive does not exist"}
-#Testing is successful.
-#>
-<#
-function Test-Existance {
-    $ValueCheck = Get-PSDrive | Where-Object {$_.root -like "HKEY_USERS"}
-        if ($ValueCheck.Root) {return 'exists'}
-        else {
-            New-PSDrive HKU Registry HKEY_USERS
-            return 'created'
-        }
-}
-#>
+
 function Test-Existance {
     $ValueCheck = Get-PSDrive | Where-Object {$_.root -like "HKEY_USERS"}
     do {
